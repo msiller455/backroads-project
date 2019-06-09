229a43891dff48a4a24dee1a3ec15c8c6b077d2c
Michael <msiller455@gmail.com>
2019-06-09 14:44:25 -0700

    adds styled component example

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout'
+import Button from '../examples/Button'
 
 export default class tours extends Component {
     render() {
         return (
             <Layout>
                 Hello from tours page!!!
+                
             </Layout>
         )
     }
