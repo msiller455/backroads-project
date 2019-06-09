d609184726661b456ffe5be17650d2fdf5aef449
Michael <msiller455@gmail.com>
2019-06-09 11:30:58 -0700

    adds styling for footer hero banner and 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import styles from '../css/error.module.css'
+import { Link } from 'gatsby'
+import Banner from '../components/Banner'
 
 export default function error() {
     return (
         <Layout>
-            Hello from error!
+            <header className={styles.error}>
+                <Banner title="oops, it's a dead dend">
+                    <Link to="/" className="btn-white">back to home page</Link>
+                </Banner>
+            </header>
         </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from '../components/Layout'
-
+import SimpleHero from '../components/SimpleHero'
+import Banner from '../components/Banner'
 
 export default () => <div>
     <Layout>
-        <div>Hello from Backroads</div>
+        <SimpleHero>
+            <Banner title="continue exploring" info="Eu et nulla id enim exercitation veniam sunt fugiat occaecat.">
+                <Link to="/tours" className="btn-white">
+                    Explore tours
+                </Link>
+            </Banner>
+        </SimpleHero>
     </Layout>
     </div>
