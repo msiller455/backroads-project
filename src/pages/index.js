554d8a8ac0bedb2215c8140d627473e519713964
Michael <msiller455@gmail.com>
2019-06-17 15:39:59 -0700

    adds aniLinks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,34 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from '../components/Layout'
-import SimpleHero from '../components/SimpleHero'
 import Banner from '../components/Banner'
 import About from '../components/Home/About'
 import Services from '../components/Home/Services'
+import StyledHero from '../components/StyledHero'
+import {graphql} from 'gatsby'
 
-export default () => <div>
+export default ({data}) => (
     <Layout>
-        <SimpleHero>
+        <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
             <Banner title="continue exploring" info="Eu et nulla id enim exercitation veniam sunt fugiat occaecat.">
                 <Link to="/tours" className="btn-white">
                     Explore tours
                 </Link>
             </Banner>
-        </SimpleHero>
+        </StyledHero>
         <About />
         <Services />
     </Layout>
-</div>
+)
+
+export const query = graphql`
+query {
+    defaultBcg:file(relativePath:{eq:"defaultBcg.jpeg"}){
+      childImageSharp{
+        fluid(quality: 90, maxWidth:4160){
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
